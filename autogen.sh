69b536c338e0cfd721b4cf2f8fba62ff4d9d2809
Hugo Villeneuve <hugo@hugovil.com>
2004-05-12 03:38:19 +0000

    Change autogen.sh script and rename test_files directory

--- autogen.sh
+++ autogen.sh
@@ -1,28 +1,68 @@
-#! /bin/sh
-# bootstrap -- Use this script to create generated files from the CVS dist
-# Copyright (C) 2000 Gary V. Vaughan
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#!/bin/sh
+# autogen.sh -- Use this script to create generated files from the SVN distribution
+# Taken from glib CVS
 
-## @start 1
-#! /bin/sh
+PROJECT=Emu8051
+TEST_TYPE=-f
+TEST_FILE=src/CPU8051.cpp
 
-set -x
-aclocal -I config
-##libtoolize --force --copy
-autoheader
-automake --add-missing --copy
+ACLOCAL_FLAGS="${ACLOCAL_FLAGS} -I config"
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd ${srcdir}
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf installed to compile $PROJECT."
+    echo "Download the appropriate package for your distribution,"
+    echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have automake installed to compile $PROJECT."
+    echo "Get ftp://sourceware.cygnus.com/pub/automake/automake-1.4.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+}
+
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have libtool installed to compile $PROJECT."
+    echo "Visit http://www.gnu.org/software/libtool/ for more information."
+    DIE=1
+}
+
+if test "${DIE}" -eq 1; then
+    exit 1
+fi
+
+test ${TEST_TYPE} ${TEST_FILE} || {
+    echo "You must run this script in the top-level $PROJECT directory"
+    exit 1
+}
+
+case ${CC} in
+    *xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
+esac
+
+aclocal ${ACLOCAL_FLAGS}
+
+#libtoolize --force --copy
+
+# Optionally feature autoheader
+(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
+
+automake --add-missing --copy ${am_opt}
+
 autoconf
-## @end 1
+
+cd ${ORIGDIR}
+
+rm -rf autom4te.cache
