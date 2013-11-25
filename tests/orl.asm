7521627a72acebb66b34cf048fa3900325f03b56
Hugo Villeneuve <hugo@hugovil.com>
2013-11-24 23:02:31 -0500

    Add tests for ORL instruction

---
+++ tests/orl.asm
@@ -0,0 +1,22 @@
+; Test program to verify correct emu8051 operation
+;
+; Test desc: ORL
+; Test output1: A = $60
+; Test output2: PSW = $E0
+; Test output3: TCON = $01
+
+        CSEG
+
+        ORG     0000h           ; Reset vector
+
+        MOV     PSW, #0E0h
+        CLR   	TCON.0
+        CLR     C
+        ORL     C, TCON.0       ; CY Should be 0
+        MOV     A, PSW
+
+        SETB   	TCON.0
+        ORL     C, TCON.0       ; CY Should be 1
+
+        LJMP    0FFF0h
+        END
