0315df659787152155a3265a3ea8c544dfd37757
Hugo Villeneuve <hugo@hugovil.com>
2013-11-24 23:14:46 -0500

    Add tests for DIV instruction

---
+++ tests/div.asm
@@ -0,0 +1,22 @@
+; Test program to verify correct emu8051 operation
+;
+; Test desc: DIV AB
+; Test output1: A = $3C
+; Test output2: B = $00
+; Test output3: PSW = $00
+
+        CSEG
+
+        ORG     0000h           ; Reset vector
+
+        MOV     A, #0FFh
+        MOV     B, #00h
+        DIV     AB              ; CY should be cleared, OV should be set
+                                ; PSW = $04
+
+        MOV     A, #240         ; 240d
+        MOV     B, PSW          ; 4d
+        DIV     AB              ; CY should be cleared, OV should be cleared, result = 10
+
+        LJMP    0FFF0h
+        END
