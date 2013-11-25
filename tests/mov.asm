28f33d3ca429287a60545f1680d5302ffbb8571d
Hugo Villeneuve <hugo@hugovil.com>
2013-11-24 23:20:18 -0500

    Add tests for MOV instruction

---
+++ tests/mov.asm
@@ -0,0 +1,22 @@
+; Test program to verify correct emu8051 operation
+;
+; Test desc: MOV
+; Test output1: A = $66
+; Test output2: B = $98
+; Test output3: PSW = $00
+; Test output4: DPTR = $9876
+; Test output5: R0 = $66
+
+        CSEG
+
+        ORG     0000h           ; Reset vector
+
+        MOV     A, #099h
+        MOV     B, A
+        DEC     B
+        MOV     R0, #066h
+        MOV     A, R0
+        MOV     DPTR,#9876h
+
+        LJMP    0FFF0h
+        END
