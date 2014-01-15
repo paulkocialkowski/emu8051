6281db4c82900528c483089a10bbe96e3d9c9907
Hugo Villeneuve <hugo@hugovil.com>
2014-01-14 22:53:08 -0500

    ChangeLog is now automatically generated from git log output

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # autogen.sh -- Use this script to create generated files from the git distribution
 
+# ChangeLog is generated from git log output using "make changelog"
+touch ChangeLog
+
 set -e
 autoreconf -vi
 rm -rf autom4te.cache
